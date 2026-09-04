4d809619dde33bc0162086b65c13ba2fa673b8d9
Edneia <yanhcesar.eds@gmail.com>
2026-09-04 20:28:36 -0300

    finalizar correçao M-sica

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ciranda Cirandinha");
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine("CIRANDA CIRANDINHA");
+Console.WriteLine("VAMOS TODOS CIRANDAR");
+Console.WriteLine("VAMOS DAR MEIA VOLTA");
+Console.WriteLine("VOLTA E MEIA VAMOS DAR");
+Console.WriteLine("O ANEL QUE TU ME DESTE");
+Console.WriteLine("ERA VIDRO E SE QUEBROU");
+Console.WriteLine("O AMOR QUE TU ME TINHAS");
+Console.WriteLine("ERA POUCO E SE ACABOU");
+Console.WriteLine();
+Console.WriteLine("POR ISSO DONA DANI");
+Console.WriteLine("FAÇA O FAVOR DE ENTRAR NA RODA");
+Console.WriteLine("DIGA UM VERSO BEM BONITO");
+Console.WriteLine("DIGA ADEUS  E VAI EMBORA");
+
